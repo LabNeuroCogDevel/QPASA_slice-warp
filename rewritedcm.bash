3de153c935c6f2f49da7641481befca97d2e8af6
WillForan <willforan@gmail.com>
2017-10-13 08:30:13 -0400

    better input parsing for rewrite bash

--- rewritedcm.bash
+++ rewritedcm.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # rewrite dicoms to look like a nifti
 # usage: rewritedcm.bash "path/to/dcmdir" "path/to/nii.gz" [ "python" ]
@@ -6,12 +7,31 @@
 [ -z "$1" -o ! -d "$1" ] && echo "bad dcm directory '$1'" && exit 1
 [ -z "$2" -o ! -r "$2" ] && echo "bad nifti '$2'"         && exit 1
 
+mkcannon() { echo $(cd $(dirname $1);pwd)/$(basename $1);}
+mkniiandimg(){
+  pwd
+  dcm2niix .
+  slicer -a img.png $(ls -1tc *nii.gz|sed 1q)
+  ls $(pwd)/img.png
+}
+
+dcmdir=$(mkcannon $1)
+niifile=$(mkcannon $2)
+
 thisdir=$(cd $(dirname $0);pwd)
-mlcmd="rewritedcm('$1','$2')"
+mlcmd="rewritedcm('$dcmdir','$niifile')"
+
+(cd $dcmdir; mkniiandimg)
 
 if [ -z "$3" ]; then
   matlab -nodisplay -r "try, addpath('$thisdir');$mlcmd;catch e, disp(e), end, quit()"
+  cd ml*/
+  mkniiandimg
+
 else
-  cd $(dirname $2)
-  $thisdir/make_dicom.py "$1" "$2"
+  cd $(dirname $niifile)
+  $thisdir/rewritedcm.py "$dcmdir" "$niifile"
+  cd pySlice*/
+  mkniiandimg
 fi
+
