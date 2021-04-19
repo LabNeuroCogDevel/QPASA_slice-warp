16f618d557ab631f50aced02208ccb5f4287195c
Will Foran <will.foran@gmail.com>
2021-04-19 13:16:24 -0400

    bug: osx needs PIL to read png?

--- gui.py
+++ gui.py
@@ -53,7 +53,15 @@ def show_mni_slice(win, show_file=None):
     """setup example slice image. implemented but unused 20210415"""
     if show_file is None:
         show_file = os.path.dirname(os.path.abspath(__file__)) + '/slice_atlas.png'
-    exslice_img = tkinter.PhotoImage(file=show_file)
+    try:
+        exslice_img = tkinter.PhotoImage(file=show_file)
+
+    except tkinter.TclError:
+        # pip install pillow
+        import PIL.Image
+        import PIL.ImageTk
+        exslice_img = PIL.ImageTk.PhotoImage(PIL.Image.open(show_file))
+
     mni_img = tkinter.Label(win)
     mni_img.image = exslice_img
     mni_img.configure(image=exslice_img)
