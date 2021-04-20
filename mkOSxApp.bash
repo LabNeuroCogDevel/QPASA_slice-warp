242b3d6f057a116f008be16bbd2625a1e6288793
WillForan <willforan@gmail.com>
2021-04-20 15:22:43 -0400

    🎨 mkOSxApp.bash: exit if already exists

--- mkOSxApp.bash
+++ mkOSxApp.bash
@@ -4,13 +4,14 @@ thisdir=$(cd $(dirname $0);pwd)
 python3=$(which python3)
 
 appdir=/Applications/slice_warp.app/Contents/MacOS/
+# dont need to do anything if it already exists
+[ -r $appdir/slice_warp ] && exit 0
+
 [ ! -d $appdir ] && mkdir -p $appdir
 
-if [ ! -r $appdir/slice_warp ]; then
- echo > $appdir/slice_warp <<HEREDOC
+echo > $appdir/slice_warp <<HEREDOC
 #!/usr/bin/osascript
 do shell script "export LC_ALL=en_US.UTF-8; export LANG=en_US.UTF-8; $python3 '$thisdir/slice_warp.py' &> /dev/null &"
 HEREDOC
 
 chmod +x $appdir/slice_warp 
-fi 
