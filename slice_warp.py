0ed13f199dae91d0ef47da6beee358db6d3a16c7
WillForan <willforan@gmail.com>
2025-09-19 11:21:30 -0400

    slice_warp.py: resolve symlink before adding path

--- slice_warp.py
+++ slice_warp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
-pkg_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "src")
+pkg_path = os.path.join(os.path.abspath(os.path.dirname(os.path.realpath(__file__))), "src")
 sys.path.append(pkg_path)
 from qpasa import slice_warp
 slice_warp.main()
